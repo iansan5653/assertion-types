cbec972f636b81899ee6f35d03bad234ddcbde67
Ian Sanders <iansan5653@gmail.com>
2020-06-01 15:38:59 -0400

    Add basic types and tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,20 +1,86 @@
 /**
- * @file **this-module** | Tests for index.ts.
+ * @file **type-assertions** | Tests for index.ts.
  * @author Ian Sanders
  * @copyright 2019 Ian Sanders
  * @license MIT
  */
 
-import * as assert from "assert";
-import * as index from "./index";
+import {Assert, Extends, Equals, NotExtends, NotEquals} from "./index";
 
-context("index.ts", function(): void {
-  describe("#getOne()", function(): void {
-    it("should return 1", function(): void {
-      assert.strictEqual(index.getOne(), 1);
-    });
-  });
-});
+// NOTE: Types are exported to avoid unused variable warnings.
 
-// This is used by pipelines to determine whether to run the test publish task
-console.log("##vso[task.setvariable variable=testsRan]true");
+// --- Shared Values ---
+interface Example {
+  a: number;
+}
+interface ExtendsExample {
+  a: 123;
+}
+type True = true;
+
+// --- Assert ---
+export type Assert_True = Assert<true>;
+// @ts-expect-error
+export type Assert_False = Assert<false>;
+// @ts-expect-error
+export type Assert_Boolean = Assert<boolean>;
+// @ts-expect-error
+export type Assert_Unknown = Assert<unknown>;
+export type Assert_AcceptsType = Assert<True>;
+
+// --- Extends ---
+export type Extends_Boolean = Assert<Extends<true, boolean>>;
+export type Extends_Numeric = Assert<Extends<123, number>>;
+export type Extends_String = Assert<Extends<"abc", string>>;
+export type Extends_Equal = Assert<Extends<42, 42>>;
+export type Extends_Interface = Assert<Extends<ExtendsExample, Example>>;
+// @ts-expect-error
+export type Extends_Unrelated = Assert<Extends<123, "abc">>;
+// @ts-expect-error
+export type Extends_Inverted = Assert<Extends<number, 123>>;
+// @ts-expect-error
+export type Extends_InvInterface = Assert<Extends<Example, ExtendsExample>>;
+
+// --- NotExtends --- (just invert all Extends tests)
+// @ts-expect-error
+export type NotExtends_Boolean = Assert<NotExtends<true, boolean>>;
+// @ts-expect-error
+export type NotExtends_Numeric = Assert<NotExtends<123, number>>;
+// @ts-expect-error
+export type NotExtends_String = Assert<NotExtends<"abc", string>>;
+// @ts-expect-error
+export type NotExtends_Equal = Assert<NotExtends<42, 42>>;
+// @ts-expect-error
+export type NotExtends_Interface = Assert<NotExtends<ExtendsExample, Example>>;
+export type NotExtends_Unrelated = Assert<NotExtends<123, "abc">>;
+export type NotExtends_Inverted = Assert<NotExtends<number, 123>>;
+export type NotExtends_InvInterface = Assert<
+  NotExtends<Example, ExtendsExample>
+>;
+
+// --- Equals ---
+export type Equals_Boolean = Assert<Equals<true, true>>;
+export type Equals_Numeric = Assert<Equals<123, 123>>;
+export type Equals_String = Assert<Equals<string, string>>;
+export type Equals_Interface = Assert<Equals<Example, Example>>;
+// @ts-expect-error
+export type Equals_Extends = Assert<Equals<ExtendsExample, Example>>;
+// @ts-expect-error
+export type Equals_InvertedExtends = Assert<Equals<Example, ExtendsExample>>;
+// @ts-expect-error
+export type Equals_Unrelated = Assert<Equals<123, "abc">>;
+
+// --- NotEquals --- (just invert all Equals tests)
+// @ts-expect-error
+export type NotEquals_Boolean = Assert<NotEquals<true, true>>;
+// @ts-expect-error
+export type NotEquals_Numeric = Assert<NotEquals<123, 123>>;
+// @ts-expect-error
+export type NotEquals_String = Assert<NotEquals<string, string>>;
+// @ts-expect-error
+export type NotEquals_Interface = Assert<NotEquals<Example, Example>>;
+export type NotEquals_Extends = Assert<NotEquals<ExtendsExample, Example>>;
+export type NotEquals_InvertedExtends = Assert<
+  NotEquals<Example, ExtendsExample>
+>;
+export type NotEquals_Unrelated = Assert<NotEquals<123, "abc">>;
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,17 @@
  * @license MIT
  */
 
-export function getOne(): number {
-  return 1;
-}
+/** The core assertion utility. Asserts that `X` is valid. */
+export type Assert<X extends true> = X;
+
+/** Tests that `A` extends `B` (`A` is assignable to `B`). */
+export type Extends<A, B> = A extends B ? true : false;
+
+/** Tests that `A` and `B` are equal (`A` extends `B` and vice versa). */
+export type Equals<A, B> = A extends B ? (B extends A ? true : false) : false;
+
+/** Tests that `A` does not extend `B`. */
+export type NotExtends<A, B> = A extends B ? false : true;
+
+/** Tests that `A` is not equal to `B`. */
+export type NotEquals<A, B> = A extends B ? (B extends A ? false : true) : true;
